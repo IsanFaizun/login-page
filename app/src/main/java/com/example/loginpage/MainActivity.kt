d62f22685c15c787fa4bd446d92b153125ecfd02
IsanFaizun <113765539+IsanFaizun@users.noreply.github.com>
2023-08-29 01:48:38 +0700

    finish the code

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.loginpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.loginpage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            var expectedEmail = "email"
+            var expectedPassword = "12345678"
+
+            loginBtn.setOnClickListener {
+                val thisEmail = email.text.toString()
+                val thisPassword = password.text.toString()
+
+                if (thisEmail == expectedEmail && thisPassword == expectedPassword) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Success",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Wrong email or password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
